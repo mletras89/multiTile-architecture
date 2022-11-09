8c2512c31cc6bcc836c4201f241c9ffde440540c
Martin Letras <martin.letras@fau.de>
2022-11-09 15:19:00 +0100

    organizing project

--- testSingleCore.java
+++ testSingleCore.java
@@ -51,79 +51,86 @@ public class testSingleCore {
     public static void main(String[] args) throws IOException {
       System.out.println("Testing Single Core Implementation!");
 
-      Tile t1 = new Tile(1,"Tile1",1);
+      Tile t1 = new Tile(1,"Tile1",1,1.0,4);
       Memory memory1 = t1.getTileLocalMemory();
       Processor cpu1 = t1.getProcessors().get(0);
 
       Actor a1 = new Actor("a1");
+      a1.setId(1) ;
       a1.setExecutionTime(10000);
       a1.setInputs(0);
       a1.setOutputs(1);
       a1.setMapping(cpu1);
 
       Actor a2 = new Actor("a2");
+      a2.setId(2) ;
       a2.setExecutionTime(10000);
       a2.setInputs(1);
       a2.setOutputs(2);
       a2.setMapping(cpu1);
 
       Actor a3 = new Actor("a3");
+      a3.setId(3) ;
       a3.setExecutionTime(10000);
       a3.setInputs(1);
       a3.setOutputs(1);
       a3.setMapping(cpu1);
 
       Actor a4 = new Actor("a4");
+      a4.setId(4) ;
       a4.setExecutionTime(10000);
       a4.setInputs(1);
       a4.setOutputs(1);
       a4.setMapping(cpu1);
 
       Actor a5 = new Actor("a5:sink");
+      a5.setId(5) ;
       a5.setExecutionTime(10000);
       a5.setInputs(2);
       a5.setOutputs(0);
       a5.setMapping(cpu1);
 
-      Fifo c1 = new Fifo(1,"c1",0,1,1000000,memory1,1,1);
-      Fifo c2 = new Fifo(2,"c2",0,1,1000000,memory1,1,1);
-      Fifo c3 = new Fifo(3,"c3",0,1,1000000,memory1,1,1);
-      Fifo c4 = new Fifo(4,"c4",0,1,1000000,memory1,1,1);
-      Fifo c5 = new Fifo(5,"c5",0,1,1000000,memory1,1,1);
+      Fifo c1 = new Fifo(1,"c1",0,1,1000000,memory1,1,1,a1,a2);
+      Fifo c2 = new Fifo(2,"c2",0,1,1000000,memory1,1,1,a2,a3);
+      Fifo c3 = new Fifo(3,"c3",0,1,1000000,memory1,1,1,a2,a4);
+      Fifo c4 = new Fifo(4,"c4",0,1,1000000,memory1,1,1,a3,a5);
+      Fifo c5 = new Fifo(5,"c5",0,1,1000000,memory1,1,1,a4,a5);
 
       Vector<Fifo> v1 = new Vector<Fifo>();
       v1.addElement(c1);
       a1.setOutputFifos(v1);
 
       Vector<Fifo> v2 = new Vector<Fifo>();
-      v2.addElement(c1);
+      v2.addElement(new Fifo(c1));
       a2.setInputFifos(v2);
 
       Vector<Fifo> v3 = new Vector<Fifo>();
-      v3.addElement(c2);
-      v3.addElement(c3);
+      v3.addElement(new Fifo(c2));
+      v3.addElement(new Fifo(c3));
       a2.setOutputFifos(v3);
 
       Vector<Fifo> v4 = new Vector<Fifo>();
-      v4.addElement(c2);
+      v4.addElement(new Fifo(c2));
       a3.setInputFifos(v4);
 
       Vector<Fifo> v5 = new Vector<Fifo>();
-      v5.addElement(c4);
+      v5.addElement(new Fifo(c4));
       a3.setOutputFifos(v5);
 
       Vector<Fifo> v6 = new Vector<Fifo>();
-      v6.addElement(c3);
+      v6.addElement(new Fifo(c3));
       a4.setInputFifos(v6);
 
       Vector<Fifo> v7 = new Vector<Fifo>();
-      v7.addElement(c5);
+      v7.addElement(new Fifo(c5));
       a4.setOutputFifos(v7);
 
       Vector<Fifo> v8 = new Vector<Fifo>();
-      v8.addElement(c4);
-      v8.addElement(c5);
+      v8.addElement(new Fifo(c4));
+      v8.addElement(new Fifo(c5));
       a5.setInputFifos(v8);
+
+
 
       Map<Integer,Fifo> fifoMap = new HashMap<Integer,Fifo>();
       fifoMap.put(1,c1);
@@ -133,9 +140,9 @@ public class testSingleCore {
       fifoMap.put(5,c5);
 
       List<Actor> actors = Arrays.asList(a1,a2,a3,a4,a5);
-      
+
       t1.setTotalIterations(1);
-      t1.runTile(actors,fifoMap);
+      t1.runTileActors(actors,fifoMap);
       t1.getProcessors().get(0).getScheduler().saveScheduleStats(".");
       t1.getCrossbar().saveCrossbarUtilizationStats(".");
       System.out.println("Testing Single Core Implementation done!");
