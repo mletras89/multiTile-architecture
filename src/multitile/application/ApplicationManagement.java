385b81af7c52bc46dd984f99272dc314e829bf70
Martin Letras <martin.letras@fau.de>
2022-11-19 13:58:04 +0100

    including method for the collapseMergeableMulticastActors

--- src/multitile/application/ApplicationManagement.java
+++ src/multitile/application/ApplicationManagement.java
@@ -42,22 +42,54 @@ package src.multitile.application;
 
 import java.util.*;
 import src.multitile.application.Actor;
-
+import src.multitile.application.Application;
+import src.multitile.application.Fifo;
+import src.multitile.application.CompositeFifo;
 
 public class ApplicationManagement{
   
-  public static Map<Integer,Actor> getMulticastActors(Application app){
-    Map<Integer,Actor> multicastActors = new HashMap<>();
+  public static Map<Integer,MulticastActor> getMulticastActors(Application app){
+    Map<Integer,MulticastActor> multicastActors = new HashMap<>();
     for(Map.Entry<Integer,Actor> actor : app.getActors().entrySet()){
       if (actor.getValue().getType() == Actor.ACTOR_TYPE.MULTICAST){
-        multicastActors.put(actor.getKey(),actor.getValue());
+        multicastActors.put(actor.getKey(),(MulticastActor)actor.getValue());
       }
     }
     return multicastActors;
   }
 
-  public static void collapseMergableMulticastActors(Application app){
-    Map<Integer,Actor> multicastActors = getMulticastActors(app);
+  // these method receives an application and returns a modified application removing 
+  // all the mergeable multicast actors from the application and replace them by composite channels
+  public static void collapseMergeableMulticastActors(Application app){
+    // get all the multicast actors
+    Map<Integer,MulticastActor> multicastActors = getMulticastActors(app);
+    
+    for(Map.Entry<Integer,MulticastActor> multicastActor : multicastActors.entrySet()){
+      MulticastActor selectedActor = multicastActor.getValue();
+      
+      if(selectedActor.isMergeMulticast() == true){
+        // if the actor is mergeable, we remove it and replace it by a composite channel
+        Vector<Fifo> inputFifos  = selectedActor.getInputFifos(); // it should be only one writer
+        Vector<Fifo> outputFifos = selectedActor.getOutputFifos(); // it might be multiple readers, more that one
+        Fifo writer = inputFifos.get(0);
+        List<Fifo>  readerFifos = new ArrayList<Fifo>(outputFifos);
+
+        CompositeFifo compositeFifo = FifoManagement.createCompositeChannel(writer,readerFifos,selectedActor); 
+        // once created the compositefifo, we have to connected into the application
+        int idWriterActor = writer.getSource().getId();
+        app.getActors().get(idWriterActor).removeOutputFifo(writer.getId());
+        // connecting the input of the composite fifo
+        app.getActors().get(idWriterActor).getOutputFifos().add(compositeFifo);
+
+        // now connect the readers to the composite fifo
+        for(Fifo dstFifo : readerFifos){
+          int idReaderActor = dstFifo.getDestination().getId();
+          app.getActors().get(idReaderActor).removeInputFifo(dstFifo.getId());
+          // connectinf the outputs of the composite fifo
+          app.getActors().get(idReaderActor).getInputFifos().add(compositeFifo);
+        }
+      }
+    }
   }
 
 }
